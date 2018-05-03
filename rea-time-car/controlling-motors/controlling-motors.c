df309a8f4476592a5d56c608b5f641ac702eea6e
Tobias Lans <lanstobias@gmail.com>
2018-05-03 15:54:20 +0200

    skriv testprogram i controlling-motors.c

--- rea-time-car/controlling-motors/controlling-motors.c
+++ rea-time-car/controlling-motors/controlling-motors.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include "Engine.h"
 
 int main(int argc, char *argv[])
 {
+	gpioSetup();
+
+	Engine engineLeft, engineRight;
+	engineInit(&engineRight, _6, _16, _20);
+	engineInit(&engineLeft, _23, _24, _25);
+
+	for (int i = 0; i < 180; i++)
+	{
+		engineRun(&engineLeft, 100, Forward);
+		engineRun(&engineRight, 100, Forward);
+	}
+
+	engineDestroy(&engineLeft);
+	engineDestroy(&engineRight);
+	
 	fflush(stdout); /* <============== Put a breakpoint here */
 	return 0;
 }
