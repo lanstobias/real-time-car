ba1f89ad9dd33b5dc4f3bec4756521ff9d945ba1
Tobias Lans <lanstobias@gmail.com>
2018-05-03 14:25:34 +0200

    skapa engineRun()

--- rea-time-car/controlling-motors/Engine.c
+++ rea-time-car/controlling-motors/Engine.c
@@ -5,6 +5,14 @@ void engineInit(Engine* engine, enum Pin speedPin, enum Pin forwardPin, enum Pin
 	
 }
 
+void engineRun(Engine* engine, int speed, Direction direction)
+{
+	pwmPulse(engine->pwm, speed);
+
+	engine->forwardPin = 1;
+	engine->reversePin = 0;
+}
+
 void engineDestroy(Engine* engine)
 {
 	
